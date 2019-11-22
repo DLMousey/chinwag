5844268e004b8a0c05339a578bcc74ff3c238665
DLMousey <tom@deadlyllama.com>
2019-11-22 00:31:26 +0000

    Dynamically generate forms, add some styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,31 @@
 import './style.css';
 
 const conn = new WebSocket('ws://localhost:8080');
+// const username = localStorage.getItem('ws_username');
 
-document.querySelector('#username_form').addEventListener('submit', (event) => {
-    console.log('username form submitted');
-    event.preventDefault();
+if (!localStorage.getItem('ws_username')) {
+    buildUsernameForm();
+}
 
-    const username = document.querySelector('#username').value;
-    document.querySelector('.floating_bar').innerHTML = "Chatting as: " + username;
-    initUsername(username);
-
-    document.querySelector('#username_form').remove();
+document.addEventListener('storage', () => {
+    if (localStorage.getItem('ws_username')) {
+        buildMessageForm();
+    } else {
+        buildUsernameForm();
+    }
 });
 
 conn.onopen = (event) => {
-    if (!localStorage.getItem('ws_identifier')) {
-        conn.send(JSON.stringify({
-            msg_type: 'request_id'
-        }));
-    }
+    const username = localStorage.getItem('ws_username');
+    if (username) {
 
-    console.log('on open!', event);
+        conn.send(JSON.stringify({
+            msg_type: 'username_init',
+            value: username
+        }));
+
+        buildMessageForm();
+    }
 };
 
 conn.onmessage = (event) => {
@@ -38,6 +43,7 @@ conn.onmessage = (event) => {
 };
 
 function initUsername(username) {
+    localStorage.setItem('ws_username', username);
     conn.send(JSON.stringify({
         msg_type: 'username_init',
         value: username
@@ -82,12 +88,71 @@ function appendMessage(data) {
     document.querySelector('.chat__messages').append(li);
 }
 
-document.querySelector('form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    let message = document.querySelector('#message').value;
-    conn.send(JSON.stringify({
-       msg_type: 'message_send',
-       value: message
-    }));
-    document.querySelector('#message').value = '';
-});
+function buildUsernameForm() {
+    const parent = document.querySelector('.form');
+    const form = document.createElement('form');
+    form.setAttribute('id', 'username_form');
+
+    const label = document.createElement('label');
+    label.setAttribute('for', 'username');
+    label.innerText = "Username:";
+
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'username');
+
+    const button = document.createElement('button');
+    button.setAttribute('type', 'submit');
+    button.innerText = "Set username";
+
+    form.appendChild(label);
+    form.appendChild(input);
+    form.appendChild(button);
+
+    form.addEventListener('submit', (event) => {
+        console.log('username form submitted');
+        event.preventDefault();
+
+        localStorage.setItem('ws_username', input.value);
+        document.querySelector('#username_form').remove();
+    });
+
+    parent.appendChild(form);
+}
+
+function buildMessageForm() {
+    const parent = document.querySelector('.form');
+    const form = document.createElement('form');
+    form.setAttribute('id', 'message_form');
+    form.classList.add('form__message');
+
+    const textarea = document.createElement('textarea');
+    textarea.setAttribute('id', 'message');
+    textarea.setAttribute('placeholder', 'Type your message and press enter to send');
+    textarea.addEventListener('keydown', (event) => {
+        if (event.keyCode === 13 && !event.shiftKey) {
+            event.preventDefault();
+            conn.send(JSON.stringify({
+                msg_type: 'message_send',
+                value: textarea.value
+            }));
+
+            textarea.setSelectionRange(0, 0);
+            textarea.value = "";
+        }
+        console.log('keydown!');
+    });
+
+    form.appendChild(textarea);
+    parent.appendChild(form);
+}
+
+// document.querySelector('#message_form').addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     let message = document.querySelector('#message').value;
+//     conn.send(JSON.stringify({
+//        msg_type: 'message_send',
+//        value: message
+//     }));
+//     document.querySelector('#message').value = '';
+// });
